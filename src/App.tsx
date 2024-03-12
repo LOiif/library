a0cb2a9efe0dadbf56b18711e73be2301c1ad657
LOiif <loiifmorner@gmail.com>
2024-03-12 20:33:38 +0300

    Подключен компонент главной страницы

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.scss';
-import Header from "./components/Header/Header";
+import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
     return (
         <div className="App">
-            <Header/>
+            <MainPage/>
         </div>
     );
 }
