7cae4cc2800422554118403fcf108ce64a911479
LOiif <loiifmorner@gmail.com>
2024-03-21 01:18:33 +0300

    Данные изменены на реальные с google books api

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../../components/Header/Header";
 import './MainPage.scss';
 import Carousel from "../../components/Carousel/Carousel";
+import axios from "axios";
 
 const MainPage = () => {
-    const items1 = ['item1', 'item2', 'item3'];
-    const items2 = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7'];
-    const items3 = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7'];
+
+    const [bookData, setData]= useState([]);
+    const API_KEY = 'AIzaSyAoXUwvsCrGZhf31no8STdw9V5YDpwP0FE';
+
+    useEffect(() => {
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=федор+достоевский:keyes&key=${API_KEY}&maxResults=8`)
+            .then(res => setData(res.data.items))
+            .catch(err => console.log(err));
+        console.log(bookData)
+    }, []);
+    useEffect(() => {
+        console.log(bookData)
+    }, [bookData]);
+
     return (
         <div>
             <Header/>
             <main className="main">
-                <Carousel items={items1} title={""} size={2}/>
+                <Carousel items={bookData} title={""} size={2}/>
                 <div className="mb"></div>
-                <Carousel items={items2} title={"Рекомендации для вас"} size={1}/>
+                <Carousel items={bookData} title={"Рекомендации для вас"} size={1}/>
                 <div className="mb"></div>
-                <Carousel items={items3} title={"Новое на сайте"} size={1}/>
+                <Carousel items={bookData} title={"Новое на сайте"} size={1}/>
                 <div className="mb"></div>
-                <Carousel items={items3} title={"Топ 10 книг за все время"} size={1}/>
+                <Carousel items={bookData} title={"Топ 10 книг за все время"} size={1}/>
                 <div className="mb"></div>
-                <Carousel items={items3} title={"Лучшее за месяц"} size={1}/>
+                <Carousel items={bookData} title={"Лучшее за месяц"} size={1}/>
             </main>
         </div>
     );
