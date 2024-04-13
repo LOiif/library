54b5fe2a6692f985bc792dbcfcdea6090d34a42a
LOiif <loiifmorner@gmail.com>
2024-04-13 13:02:12 +0300

    базовая разметка для страницы с инф. о книге

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -6,32 +6,49 @@ import axios from "axios";
 
 const MainPage = () => {
 
-    const [bookData, setData]= useState([]);
+    const [mainBookData, setMainData] = useState([]);
+    const [recommendationsData, setRecommendationsData] = useState([]);
+    const [newData, setNewData] = useState([]);
+    const [bestData, setBestData] = useState([]);
+    const [topData, setTopData] = useState([]);
     const API_KEY = 'AIzaSyAoXUwvsCrGZhf31no8STdw9V5YDpwP0FE';
 
+    const fetchData = uri => axios.get(uri).then(({data}) => data);
     useEffect(() => {
-        axios.get(`https://www.googleapis.com/books/v1/volumes?q=федор+достоевский:keyes&key=${API_KEY}&maxResults=8`)
-            .then(res => setData(res.data.items))
-            .catch(err => console.log(err));
-        console.log(bookData)
+        Promise.all([
+            fetchData(`https://www.googleapis.com/books/v1/volumes?q=intitle:Одноэтажная+Америка&key=${API_KEY}&maxResults=1&langRestrict=ru`),
+            fetchData(`https://www.googleapis.com/books/v1/volumes?q=intitle:Война+и+мир+лев+толстой&key=${API_KEY}&maxResults=1&langRestrict=ru`),
+            fetchData(`https://www.googleapis.com/books/v1/volumes?q=intitle:Тихий+дон&key=${API_KEY}&maxResults=1&langRestrict=ru`),
+            fetchData(`https://www.googleapis.com/books/v1/volumes?q=intitle:Двенадцать+стульев&key=${API_KEY}&maxResults=1&langRestrict=ru`),
+            fetchData(`https://www.googleapis.com/books/v1/volumes?q=intitle:Дюна&key=${API_KEY}&maxResults=1&langRestrict=ru`),
+            ]).then(([ data1, data2, data3,data4, data5 ]) => {
+            setMainData([...data1.items, ...data2.items, ...data3.items, ...data4.items, ...data5.items])
+        }).catch((err) => console.log(err))
+
+        Promise.all([
+            fetchData(`https://www.googleapis.com/books/v1/volumes?q=intitle:Война+и+мир&key=${API_KEY}&maxResults=20&langRestrict=ru`),
+        ]).then(([ data1]) => {
+            setRecommendationsData(data1.items)
+        }).catch((err) => console.log(err))
     }, []);
+
     useEffect(() => {
-        console.log(bookData)
-    }, [bookData]);
+        console.log(mainBookData)
+    }, [mainBookData]);
 
     return (
         <div>
             <Header/>
             <main className="main">
-                <Carousel items={bookData} title={""} size={2}/>
+                <Carousel items={mainBookData} title={""} size={2}/>
                 <div className="mb"></div>
-                <Carousel items={bookData} title={"Рекомендации для вас"} size={1}/>
+                <Carousel items={recommendationsData} title={"Рекомендации для вас"} size={1}/>
                 <div className="mb"></div>
-                <Carousel items={bookData} title={"Новое на сайте"} size={1}/>
+                <Carousel items={recommendationsData} title={"Новое на сайте"} size={1}/>
                 <div className="mb"></div>
-                <Carousel items={bookData} title={"Топ 10 книг за все время"} size={1}/>
+                <Carousel items={recommendationsData} title={"Топ 10 книг за все время"} size={1}/>
                 <div className="mb"></div>
-                <Carousel items={bookData} title={"Лучшее за месяц"} size={1}/>
+                <Carousel items={recommendationsData} title={"Лучшее за месяц"} size={1}/>
             </main>
         </div>
     );
