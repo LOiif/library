4fd80ba6d68c9212c1a0c3e8472657eed01468d7
LOiif <loiifmorner@gmail.com>
2024-05-11 14:46:12 +0300

    Сделана серверная ччасть авторизации

---
+++ server/index.ts
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+const router = require('./router/index')
+const errorMiddleware = require('./middlewares/error-middleware')
+
+const app = express()
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
+app.use('/api', router)
+app.use(errorMiddleware)
+
+
+const PORT = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL)
+        app.listen(PORT, () => {
+            console.log(`Started on ${PORT} port`)
+        })
+    } catch (e: any) {
+        console.log(e)
+    }
+}
+
+start()
