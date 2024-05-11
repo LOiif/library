b97b19cd09e3bfce63730e49298440f7942bd940
LOiif <loiifmorner@gmail.com>
2024-05-11 14:55:48 +0300

    реализована логика авторизации и запрсов на клиенте

---
+++ client/src/store/store.ts
@@ -0,0 +1,94 @@
+import {IUser} from "../models/IUser";
+import {makeAutoObservable} from "mobx";
+import AuthService from "../services/AuthService";
+import axios from 'axios';
+import {AuthResponse} from "../models/response/AuthResponse";
+import {API_URL} from "../http";
+
+export default class Store {
+    user = {} as IUser;
+    isAuth = false;
+    isLoading = false;
+
+    loginError = null;
+    registrationError = null;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    setLoginError(val) {
+        this.loginError = val;
+    }
+
+    setRegistrationError(val) {
+        this.registrationError = val;
+    }
+
+    setAuth(bool: boolean) {
+        this.isAuth = bool;
+    }
+
+    setUser(user: IUser) {
+        this.user = user;
+    }
+
+    setLoading(bool: boolean) {
+        this.isLoading = bool;
+    }
+
+    async login(email: string, password: string) {
+        try {
+            const response = await AuthService.login(email, password);
+            console.log(response)
+            this.setLoginError(null);
+            localStorage.setItem('token', response.data.accessToken);
+            this.setAuth(true);
+            this.setUser(response.data.user);
+
+        } catch (e) {
+            this.setLoginError(e.response?.data);
+            console.log(e.response?.data?.message);
+        }
+    }
+
+    async registration(email: string, password: string) {
+        try {
+            const response = await AuthService.registration(email, password);
+            this.setRegistrationError(null);
+            localStorage.setItem('token', response.data.accessToken);
+            this.setAuth(true);
+            this.setUser(response.data.user);
+
+        } catch (e) {
+            this.setRegistrationError(e.response?.data);
+            console.log(e.response?.data);
+        }
+    }
+
+    async logout() {
+        try {
+            const response = await AuthService.logout();
+            localStorage.removeItem('token');
+            this.setAuth(false);
+            this.setUser({} as IUser);
+        } catch (e) {
+            console.log(e.response?.data?.message);
+        }
+    }
+
+    async checkAuth() {
+        this.setLoading(true);
+        try {
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
+            console.log(response);
+            localStorage.setItem('token', response.data.accessToken);
+            this.setAuth(true);
+            this.setUser(response.data.user);
+        } catch (e) {
+            console.log(e.response?.data?.message);
+        } finally {
+            this.setLoading(false);
+        }
+    }
+}
