8e04062b16745f9ced4a31a1772f1089a2ac7897
LOiif <loiifmorner@gmail.com>
2024-05-15 01:44:01 +0300

    Реализована карусель в главном меню

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,51 +1,83 @@
 import React, {FC} from 'react';
+import 'react-multi-carousel/lib/styles.css';
 import "./Carousel.scss";
 import {ReactComponent as LeftArrowIcon} from "../../images/arrow_left.svg";
 import {ReactComponent as RightArrowIcon} from "../../images/arrow_right.svg";
 import Card from "../Card/Card";
+import Carousel from 'react-multi-carousel';
 
-const Carousel: FC<any> = ({items, title, size}) => {
+const MyCarousel: FC<any> = ({items, title, size}) => {
     let itemClass = 'item';
     let buttonClass = 'button'
+
+    let deskItems = 7;
+    let tabletItems = 5;
+    let mobileItems = 3;
+
     switch (size) {
         case 1:
             buttonClass += ' button--small'
             itemClass += ' item--small'
+            deskItems = 7;
+            tabletItems = 4;
+            mobileItems = 2;
             break;
         case 2:
             buttonClass += ' button--big'
             itemClass += ' item--big'
+            deskItems = 5;
+            tabletItems = 2;
+            mobileItems = 1;
             break;
     }
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: {max: 4000, min: 3000},
+            items: deskItems
+        },
+        desktop: {
+            breakpoint: {max: 3000, min: 1024},
+            items: deskItems
+        },
+        tablet: {
+            breakpoint: {max: 1024, min: 464},
+            items: tabletItems
+        },
+        mobile: {
+            breakpoint: {max: 464, min: 0},
+            items: mobileItems
+        }
+    };
     return (
         <>
             {
                 (title) ? <h2 className="title">{title}</h2>
                     : <></>
             }
-            <div className="carousel-container">
-                <button className={buttonClass + " prev-button"}>
-                    <LeftArrowIcon/>
-                    Назад
-                </button>
-                <ul className="items-list">
-                    {
-                        size == 1
-                            ? items.slice(0, 7).map((item) => <li key={item.id} className={itemClass}>
-                                <Card bookInfo={item}/>
-                            </li>)
-                            : items.slice(0, 5).map((item) => <li key={item.id} className={itemClass}>
-                                <Card bookInfo={item}/>
-                            </li>)
-                    }
-                </ul>
-                <button className={buttonClass + " next-button"}>
-                    <RightArrowIcon/>
-                    Вперед
-                </button>
-            </div>
+            <Carousel responsive={responsive}
+                      itemClass={itemClass}
+                      infinite={true}
+                      containerClass={size === 2 ? "carousel-container--big" : "carousel-container--small"}
+                      removeArrowOnDeviceType={["tablet", "mobile"]}
+                      partialVisible={false}
+                      customLeftArrow={<button className={buttonClass + " prev-button"}>
+                          <LeftArrowIcon/>
+                          Назад
+                      </button>}
+                      customRightArrow={<button className={buttonClass + " next-button"}>
+                          <RightArrowIcon/>
+                          Вперед
+                      </button>}
+            >
+                {
+                    items.map((item) => <div key={item.id} className={itemClass}>
+                        <Card bookInfo={item}/>
+                    </div>)
+                }
+            </Carousel>
         </>
     );
 };
 
-export default Carousel;
+export default MyCarousel;
--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header/Header";
 import './MainPage.scss';
 import Carousel from "../../components/Carousel/Carousel";
 import axios from "axios";
+import MyCarousel from "../../components/Carousel/Carousel";
 
 const MainPage = () => {
 
@@ -21,13 +22,13 @@ const MainPage = () => {
             fetchData(`https://www.googleapis.com/books/v1/volumes?q=intitle:Тихий+дон&key=${API_KEY}&maxResults=1&langRestrict=ru`),
             fetchData(`https://www.googleapis.com/books/v1/volumes?q=intitle:Двенадцать+стульев&key=${API_KEY}&maxResults=1&langRestrict=ru`),
             fetchData(`https://www.googleapis.com/books/v1/volumes?q=intitle:Дюна&key=${API_KEY}&maxResults=1&langRestrict=ru`),
-            ]).then(([ data1, data2, data3,data4, data5 ]) => {
-            setMainData([...data1.items, ...data2.items, ...data3.items, ...data4.items, ...data5.items])
+        ]).then(([data1, data2, data3, data4, data5]) => {
+            setMainData([...data1.items, ...data2.items, ...data3.items, ...data4.items, ...data5.items, ...data5.items, ...data5.items])
         }).catch((err) => console.log(err))
 
         Promise.all([
             fetchData(`https://www.googleapis.com/books/v1/volumes?q=intitle:Война+и+мир&key=${API_KEY}&maxResults=20&langRestrict=ru`),
-        ]).then(([ data1]) => {
+        ]).then(([data1]) => {
             setRecommendationsData(data1.items)
         }).catch((err) => console.log(err))
     }, []);
@@ -40,15 +41,15 @@ const MainPage = () => {
         <div>
             <Header/>
             <main className="main">
-                <Carousel items={mainBookData} title={""} size={2}/>
+                <MyCarousel items={mainBookData} title={""} size={2}/>
                 <div className="mb"></div>
-                <Carousel items={recommendationsData} title={"Рекомендации для вас"} size={1}/>
+                <MyCarousel items={recommendationsData} title={"Рекомендации для вас"} size={1}/>
                 <div className="mb"></div>
-                <Carousel items={recommendationsData} title={"Новое на сайте"} size={1}/>
+                <MyCarousel items={recommendationsData} title={"Новое на сайте"} size={1}/>
                 <div className="mb"></div>
-                <Carousel items={recommendationsData} title={"Топ 10 книг за все время"} size={1}/>
+                <MyCarousel items={recommendationsData} title={"Топ 10 книг за все время"} size={1}/>
                 <div className="mb"></div>
-                <Carousel items={recommendationsData} title={"Лучшее за месяц"} size={1}/>
+                <MyCarousel items={recommendationsData} title={"Лучшее за месяц"} size={1}/>
             </main>
         </div>
     );
