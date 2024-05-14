5ce6f25cb5cb1e9d933aa32d3e72c175d589d64e
LOiif <loiifmorner@gmail.com>
2024-05-14 03:16:36 +0300

    Добавлен блок с комментариями, и кнопка закрыть у уведомления

--- client/src/components/Notification/Notification.tsx
+++ client/src/components/Notification/Notification.tsx
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 import styles from './Notification.module.scss'
-
+import crossSvg from '../../images/cross.svg'
 const Notification = ({message='Ошибка', show=false, closeNotification}) => {
 
 
     return show ? (
         <div className={styles.container}>
             <p className={styles.message}>{message}</p>
-            <button className={styles.closeButton} onClick={closeNotification}>Закрыть</button>
+            <button className={styles.closeButton} onClick={closeNotification} style={{backgroundImage: `url(${crossSvg})`}}>Закрыть</button>
         </div>
     ) : <></>
 };
--- client/src/pages/BookDetails/BookDetails.tsx
+++ client/src/pages/BookDetails/BookDetails.tsx
@@ -1,10 +1,11 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useId, useState} from 'react';
 import {useLocation} from 'react-router-dom'
 import Header from "../../components/Header/Header";
 import styles from "./BookDetails.module.scss"
 import {ReactComponent as Heart} from "../../images/favourites.svg";
 import {Context} from "../../index";
 import Notification from "../../components/Notification/Notification";
+import {randomBytes} from "crypto";
 
 const BookDetails = () => {
     const location = useLocation()
@@ -18,17 +19,30 @@ const BookDetails = () => {
             store.checkAuth()
         }
     }, [])
-    const addFavourite = (e) => {
 
-        if (store.isAuth){
+    const [timeOutId, setTimeoutId] = useState(null);
 
-             store.addFavourites(store.getUser().id, bookInfo.id)
-
-        } else {
-            setShowNotification(true);
-            setTimeout(() => {
+    useEffect(() => {
+        if (timeOutId) {
+            clearTimeout(timeOutId);
+        }
+        if (showNotification === true) {
+            setTimeoutId(setTimeout(() => {
                 setShowNotification(false)
-            }, 3000)
+            }, 3000))
+        }
+    }, [showNotification])
+    const addFavourite = (e) => {
+        if (store.isAuth && store.user.isActivated) {
+            store.addFavourites(store.getUser().id, bookInfo.id)
+        } else {
+            if (timeOutId) {
+                clearTimeout(timeOutId);
+            }
+            setShowNotification(true);
+            setTimeoutId(setTimeout(() => {
+                setShowNotification(false)
+            }, 3000))
         }
     }
 
@@ -39,7 +53,8 @@ const BookDetails = () => {
     console.log(bookInfo)
     return (
         <>
-            <Notification message={'Необходимо авторизоваться'} show={showNotification} closeNotification={closeNotification}></Notification>
+            <Notification message={'Необходимо авторизоваться'} show={showNotification}
+                          closeNotification={closeNotification}></Notification>
             <Header/>
             <main className={styles.main}>
                 <div className={styles.buttonContainer} onClick={addFavourite}>
@@ -49,31 +64,41 @@ const BookDetails = () => {
                 <span className={styles.line}/>
                 <div className={styles.container}>
                     <img className={styles.img} src={thumbnail} alt={'Картинка кинги'}/>
-                    <div className={styles.bookInfo}>
-                        {
-                            bookInfo.volumeInfo.title ?
-                                <p className={styles.bookTitle}>{bookInfo.volumeInfo.title}</p>
-                                : <></>
-                        }
+                    <div className={styles.contentContainer}>
+                        <div className={styles.bookInfo}>
+                            {
+                                bookInfo.volumeInfo.title ?
+                                    <p className={styles.bookTitle}>{bookInfo.volumeInfo.title}</p>
+                                    : <></>
+                            }
 
-                        {
-                            bookInfo.volumeInfo.authors.length !== 0 ?
-                                <p className={styles.authors}>
-                                    Авторы: {
-                                    bookInfo.volumeInfo.authors.map((author, id) =>
-                                        (id < bookInfo.volumeInfo.authors.length - 1)
-                                            ? <span key={Date.now()}>{`${author}, `}</span>
-                                            : <span key={Date.now()}>{author}</span>
-                                    )
-                                }
-                                </p>
-                                : <></>
-                        }
-                        {
-                            bookInfo.volumeInfo.description ?
-                                <p className={styles.description}>{bookInfo.volumeInfo.description}</p>
-                                : <></>
-                        }
+                            {
+                                bookInfo.volumeInfo.authors.length !== 0 ?
+                                    <p className={styles.authors}>
+                                        Авторы: {
+                                        bookInfo.volumeInfo.authors.map((author, id) =>
+                                            (id < bookInfo.volumeInfo.authors.length - 1)
+                                                ? <span key={author}>{`${author}, `}</span>
+                                                : <span key={author}>{author}</span>
+                                        )
+                                    }
+                                    </p>
+                                    : <></>
+                            }
+                            {
+                                bookInfo.volumeInfo.description ?
+                                    <p className={styles.description}>{bookInfo.volumeInfo.description}</p>
+                                    : <></>
+                            }
+                        </div>
+                        <div className={styles.comments}>
+                            <h3 className={styles.commentsTitle}>Отзывы 1</h3>
+                            {
+                                store.isAuth && store.user?.isActivated 
+                                    ? <textarea className={styles.commentsInput}/>
+                                    : <p className={styles.notAuthMessage}>Авторизуйтесь, чтобы оставить комментарий</p>
+                            }
+                        </div>
                     </div>
                 </div>
             </main>
